d2e6b5dc6683e00049945b1c8a83e30d1ad7859e
Mark Laramee <marklaramee@gmail.com>
2023-04-10 10:44:24 -0700

    started sub nav

--- components/navigation/Navigation.tsx
+++ components/navigation/Navigation.tsx
@@ -4,12 +4,19 @@ import Link from 'next/link'
 const Navigation = () => {
     return (
         <nav className={styles.navTop}>
-            <Link href="/">HOME</Link>
-            <a href="#">APPS</a>
-            <a href="#">MUSIC</a>
+            <div className={styles.navLevel1}>
+                <Link href="/">HOME</Link>
+            </div>
+            <div className={styles.navLevel1}>
+                <a href="#">APPS</a>
+            </div>
+            <div className={styles.navLevel1}>
+                <a href="#">MUSIC</a>
+            </div>
             <div className={styles.navLevel1}>
                 <span className={styles.navLevel1}>WORK</span>
                 <div className={styles.navLevel2}>
+                    <a href="https://www.linkedin.com/in/marklaramee" target="_blank">Linked In</a>
                 </div>
             </div>
         </nav>
