b29e8c04028e8a73f3d134b107c99480891e3a7f
Mark Laramee <marklaramee@gmail.com>
2023-04-10 11:05:43 -0700

    work subnav

--- components/navigation/Navigation.tsx
+++ components/navigation/Navigation.tsx
@@ -7,16 +7,19 @@ const Navigation = () => {
             <div className={styles.navLevel1}>
                 <Link href="/">HOME</Link>
             </div>
-            <div className={styles.navLevel1}>
+            {/* <div className={styles.navLevel1}>
                 <a href="#">APPS</a>
             </div>
             <div className={styles.navLevel1}>
                 <a href="#">MUSIC</a>
-            </div>
+            </div> */}
             <div className={styles.navLevel1}>
                 <span className={styles.navLevel1}>WORK</span>
                 <div className={styles.navLevel2}>
                     <a href="https://www.linkedin.com/in/marklaramee" target="_blank">Linked In</a>
+                    <a href="https://github.com/marklaramee" target="_blank">Github</a>
+                    <a href="/content/Mark-Laramee-Resume.pdf" target="_blank">Resume - Skills</a>
+                    <a href="/content/Mark-Laramee-CV.pdf" target="_blank">Resume - Accomplishments</a>
                 </div>
             </div>
         </nav>
