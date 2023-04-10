381a5ae2756a43e8038452b3e072ca77903d3940
Mark Laramee <marklaramee@gmail.com>
2023-04-09 23:59:44 -0700

    cleanup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import PageTop from '@/components/pageTop/PageTop'
-import { Roboto } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-
-
-// import 'https://fonts.googleapis.com/css?family=Roboto:400,700';
-
-const roboto = Roboto({
-  weight: '400',
-  preload: false,
-})
 
 export default function Home() {
   return (
