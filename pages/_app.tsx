131faae63af9698bd253b5537db44c5ed750686e
Mark Laramee <marklaramee@gmail.com>
2023-04-15 15:34:12 -0700

    moved layout items to app component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,23 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import Image from 'next/image'
+import PageTop from '@/components/PageTop/PageTop'
+import styles from '@/styles/Home.module.css'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Mark Laramee: Home</title>
+        <meta name="description" content="Mark Laramee" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={styles.main}>
+        <PageTop />
+        <Component {...pageProps} />
+      </main>
+    </>
+  )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,23 +6,14 @@ import styles from '@/styles/Home.module.css'
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Mark Laramee: Home</title>
-        <meta name="description" content="Mark Laramee" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className={styles.main}>
-        <PageTop />
-        <div className={styles.construction}></div>
+      <div className={styles.construction}></div>
         <div style={{width: '100%', height: '600px', position: 'relative'}}>
           <Image
             alt='Broken TV'
             src='/images/broken-tv-4.png'
             fill
           />
-        </div>
-      </main>
+      </div>
     </>
   )
 }
