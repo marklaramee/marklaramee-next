d905cfaf19f69928bbd9e65e7cf54258b1a79fee
Mark Laramee <marklaramee@gmail.com>
2023-04-16 01:00:53 -0700

    adding correct form elements

--- components/ContactForm/ContactForm.tsx
+++ components/ContactForm/ContactForm.tsx
@@ -1,13 +1,22 @@
-import styles from './styles/Contact.module.css';
+import styles from './styles/ContactForm.module.css';
 
 const ContactForm = () => {
     return (
-        <form action="/contact-action" method="post">
-            <label htmlFor="first">First name:</label>
-            <input type="text" id="first" name="first" />
-            <label htmlFor="last">Last name:</label>
-            <input type="text" id="last" name="last" />
-            <button type="submit">Submit</button>
+        <form action='/contact-action' method='post' className={styles.contactForm}>
+            <label htmlFor='contactName'>Your Name:
+                <input type='text' id='contactName' name='contactName' />
+            </label>
+            <label htmlFor='contactEmail'>Your Email Address:
+                <input type='text' id='contactEmail' name='contactEmail' />
+            </label>
+            <label htmlFor='subject'>Subject (optional):
+                <input type='text' id='subject' name='subject' />
+            </label>
+            <label htmlFor='message'>Message:
+                <input type='text' id='message' name='message' />
+            </label>
+            
+            <button type='submit'>Contact Me</button>
         </form>
     );
 };
