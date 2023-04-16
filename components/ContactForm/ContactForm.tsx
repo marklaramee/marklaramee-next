b682dac6d8144fada2b6316adc9190b13f5b0417
Mark Laramee <marklaramee@gmail.com>
2023-04-16 03:05:31 -0700

    more styling

--- components/ContactForm/ContactForm.tsx
+++ components/ContactForm/ContactForm.tsx
@@ -4,32 +4,32 @@ import styles from './styles/ContactForm.module.css';
 
 const ContactForm = () => {
     return (
-        <>
-        <form action='/contact-action' method='post' className={styles.contactForm}>
-            <label htmlFor='contactName' className={styles.textContainer}>
-                <span>Your Name:</span>
-                <input type='text' id='contactName' name='contactName' />
-            </label>
-            <label htmlFor='contactEmail' className={styles.textContainer}>
-                <span>Your Email Address:</span>
-                <input type='text' id='contactEmail' name='contactEmail' />
-            </label>
-            <label htmlFor='subject' className={styles.textContainer}>
-                <span>Subject (optional):</span>
-                <input type='text' id='subject' name='subject' />
-            </label>
-            <label htmlFor='message' className={styles.textAreaContainer}>
-                <div className={styles.textAreaDiv}>Message:</div>
-                <div className='growWrap'>
-                    <textarea id='message' name='message'>
-                    </textarea>
+        <div className={styles.wrapper}>
+            <form action='/contact-action' method='post' className={styles.contactForm}>
+                <label htmlFor='contactName' className={styles.textContainer}>
+                    <span>Your Name:</span>
+                    <input type='text' id='contactName' name='contactName' />
+                </label>
+                <label htmlFor='contactEmail' className={styles.textContainer}>
+                    <span>Your Email Address:</span>
+                    <input type='text' id='contactEmail' name='contactEmail' />
+                </label>
+                <label htmlFor='subject' className={styles.textContainer}>
+                    <span>Subject (optional):</span>
+                    <input type='text' id='subject' name='subject' />
+                </label>
+                <label htmlFor='message' className={styles.textAreaContainer}>
+                    <div className={styles.textAreaDiv}>Message:</div>
+                    <div className='growWrap'>
+                        <textarea id='message' name='message'>
+                        </textarea>
+                    </div>
+                </label>
+                <div className={styles.buttonContainer}>
+                    <button type='submit' className='reset'>Contact Me</button>
                 </div>
-            </label>
-            <div className={styles.buttonContainer}>
-                <button type='submit' className='reset'>Contact Me</button>
-            </div>
-        </form>
-        </>
+            </form>
+        </div>
     );
 };
 
--- components/TopNavigation/TopNavigation.tsx
+++ components/TopNavigation/TopNavigation.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import styles from './styles/TopNavigation.module.scss';
 import Link from 'next/link'
 import classnames from 'classnames';
