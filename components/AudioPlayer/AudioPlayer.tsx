4641e4c34677f89dfc1bcd7abf4fbafda14d35cc
Mark Laramee <marklaramee@gmail.com>
2023-04-21 01:08:57 -0700

    cleaned up audioplayer

--- components/AudioPlayer/AudioPlayer.tsx
+++ components/AudioPlayer/AudioPlayer.tsx
@@ -6,82 +6,45 @@ import EditableProgressBar from '../EditableProgressBar/EditableProgressBar';
 
 // https://blog.logrocket.com/building-audio-player-react/#configuring-progress-bar-volume-slider
 // https://stackoverflow.com/questions/47686345/playing-sound-in-react-js
-
 // https://hearnow.com/preview/g1Df19HaF7g1e3OhdM0n0g%3D%3D?cid=17417
 
 export interface props {
     url: string; 
-    // hamburgerClick: () => void;
 }
 
 
 const AudioPlayer = ({url }: props) => {
-
-
   // TODO: use dynamic prop value
   const songSeconds = 312;
   
-
-
   // hooks
-  // const [audio] = useState(new Audio(url));
   const [isPlaying, setIsPlaying] = useState(false);
   const [currentSeconds, setCurrentSeconds] = useState(0);
   const toggle = () => setIsPlaying(!isPlaying);
   const audioRef = useRef<HTMLAudioElement>(null);
   const playAnimationRef = useRef(0);
 
-  
-
-  // console.log(audioRef);
-
-  // const repeat = useCallback(() => {
-  //   console.log('run');
-
-  //   playAnimationRef.current = requestAnimationFrame(repeat);
-  //   console.log(playAnimationRef.current);
-  // }, [playAnimationRef]);
-
   useEffect(() => {
     if (audioRef.current) {
       if (isPlaying) {
         audioRef.current.play();
-        // playAnimationRef.current = requestAnimationFrame(repeat);
       } else {
         audioRef.current.pause();
-        // cancelAnimationFrame(playAnimationRef.current);
       }
     }
   }, [isPlaying, audioRef]);
 
   const updateAudioTime = (newTime: string) => {
-    // console.log(newTime);
     if (audioRef.current) {
       const percentage = parseInt(newTime);
       audioRef.current.currentTime = getPercentage(percentage, songSeconds);
     }
   }
 
-  // useEffect(() => {
-  //     playing ? audio.play() : audio.pause();
-  //   },
-  //   [playing, audio]
-  // );
-
-  // useEffect(() => {
-  //   audio.addEventListener('ended', () => setPlaying(false));
-  //   return () => {
-  //     audio.removeEventListener('ended', () => setPlaying(false));
-  //   };
-  // }, [audio]);
-
   const timeUpdate = (event: any) => {
-        const seconds = Math.floor(event.target.currentTime);
-        // const currentTime = str_pad_left(minutes,'0',2) + ':' + str_pad_left(seconds,'0',2);
-        // setCurrentTime(currentTime);
-        console.log(seconds);
-        setCurrentSeconds(seconds);
-    }
+      const seconds = Math.floor(event.target.currentTime);
+      setCurrentSeconds(seconds);
+  }
 
   return (
     <div className={styles.audioContainer}>
