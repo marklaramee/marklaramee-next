510f375fdcf31960138cc9c7d1cabbed24ff9839
Mark Laramee <marklaramee@gmail.com>
2024-10-04 10:34:29 -0700

    added sass script to npm

---
+++ pages/music/featured/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import classnames from 'classnames';
+import MusicGroup from '@/components/MusicGroup/MusicGroup';
+import styles from './styles/dts.module.css';
+import MusicItem from '@/components/MusicItem/MusicItem';
+import { songsData } from '@/data/musicData';
+
+const Featured = () => {
+    return (
+        <div className={styles.musicOuter}>
+            <h1 className={classnames(styles.headline, styles.musicTitle)}>Newest Release</h1>
+            <div className={styles.musicContainer}>
+                <MusicItem song={songsData[0]} key={songsData[0].cover}/>
+            </div>
+        </div>
+    );
+}
+
+export default Featured;
