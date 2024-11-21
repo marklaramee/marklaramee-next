482e20985066342c1319bbc4bba04fa2931ffc0d
Mark Laramee <marklaramee@gmail.com>
2024-11-21 09:27:24 -0800

    hiding work tag

--- components/TopNavigation/TopNavigation.tsx
+++ components/TopNavigation/TopNavigation.tsx
@@ -20,15 +20,15 @@ const TopNavigation = ({isOpen, navLinkClick}: props) => {
             <div className={styles.navLevel1}>
                 <Link href="/music" onClick={navLinkClick}>MUSIC</Link>
             </div> 
-            <div className={styles.navLevel1}>
+            {/* <div className={styles.navLevel1}>
                 <span className={styles.navLevel1}>WORK</span>
                 <div className={styles.navLevel2}>
                     <a href="https://www.linkedin.com/in/marklaramee" target="_blank" onClick={navLinkClick}>Linked In</a>
                     <a href="https://github.com/marklaramee" target="_blank" onClick={navLinkClick}>Github</a>
-                    {/* <a href="/content/Mark-Laramee-Resume.pdf" target="_blank" onClick={navLinkClick}>Resume - Skills</a>
-                    <a href="/content/Mark-Laramee-CV.pdf" target="_blank" onClick={navLinkClick}>Resume - Accomplishments</a> */}
+                    <a href="/content/Mark-Laramee-Resume.pdf" target="_blank" onClick={navLinkClick}>Resume - Skills</a>
+                    <a href="/content/Mark-Laramee-CV.pdf" target="_blank" onClick={navLinkClick}>Resume - Accomplishments</a>
                 </div>
-            </div>
+            </div> */}
             {/* <div className={styles.navLevel1}>
                 <Link href="/travel">Travel</Link>
             </div> */}
