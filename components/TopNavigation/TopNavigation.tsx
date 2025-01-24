15669b6a109ba5fd3c636d2bb39465f874f0669c
Mark Laramee <marklaramee@gmail.com>
2025-01-24 11:08:50 -0600

    added mroe centering and padding bottom

--- components/TopNavigation/TopNavigation.tsx
+++ components/TopNavigation/TopNavigation.tsx
@@ -10,32 +10,34 @@ export interface props {
 
 const TopNavigation = ({isOpen, navLinkClick}: props) => {
     return (
-        <nav className={classnames(styles.navTop, {[styles.mobileOpen]: isOpen})}>
-            <div className={styles.navLevel1}>
-                <Link href="/" onClick={navLinkClick}>HOME</Link>
-            </div>
-            {/* <div className={styles.navLevel1}>
-                <a href="#">APPS</a>
-            </div>*/}
-            <div className={styles.navLevel1}>
-                <Link href="/music" onClick={navLinkClick}>MUSIC</Link>
-            </div> 
-            {/* <div className={styles.navLevel1}>
-                <span className={styles.navLevel1}>WORK</span>
-                <div className={styles.navLevel2}>
-                    <a href="https://www.linkedin.com/in/marklaramee" target="_blank" onClick={navLinkClick}>Linked In</a>
-                    <a href="https://github.com/marklaramee" target="_blank" onClick={navLinkClick}>Github</a>
-                    <a href="/content/Mark-Laramee-Resume.pdf" target="_blank" onClick={navLinkClick}>Resume - Skills</a>
-                    <a href="/content/Mark-Laramee-CV.pdf" target="_blank" onClick={navLinkClick}>Resume - Accomplishments</a>
+        <div className={styles.navOuter}>
+            <nav className={classnames(styles.navTop, {[styles.mobileOpen]: isOpen})}>
+                <div className={styles.navLevel1}>
+                    <Link href="/" onClick={navLinkClick}>HOME</Link>
                 </div>
-            </div> */}
-            {/* <div className={styles.navLevel1}>
-                <Link href="/travel">Travel</Link>
-            </div> */}
-            {/* <div className={styles.navLevel1}>
-                <Link href="/contact" onClick={navLinkClick}>Contact</Link>
-            </div>*/}
-        </nav> 
+                {/* <div className={styles.navLevel1}>
+                    <a href="#">APPS</a>
+                </div>*/}
+                <div className={styles.navLevel1}>
+                    <Link href="/music" onClick={navLinkClick}>MUSIC</Link>
+                </div> 
+                {/* <div className={styles.navLevel1}>
+                    <span className={styles.navLevel1}>WORK</span>
+                    <div className={styles.navLevel2}>
+                        <a href="https://www.linkedin.com/in/marklaramee" target="_blank" onClick={navLinkClick}>Linked In</a>
+                        <a href="https://github.com/marklaramee" target="_blank" onClick={navLinkClick}>Github</a>
+                        <a href="/content/Mark-Laramee-Resume.pdf" target="_blank" onClick={navLinkClick}>Resume - Skills</a>
+                        <a href="/content/Mark-Laramee-CV.pdf" target="_blank" onClick={navLinkClick}>Resume - Accomplishments</a>
+                    </div>
+                </div> */}
+                {/* <div className={styles.navLevel1}>
+                    <Link href="/travel">Travel</Link>
+                </div> */}
+                {/* <div className={styles.navLevel1}>
+                    <Link href="/contact" onClick={navLinkClick}>Contact</Link>
+                </div>*/}
+            </nav> 
+        </div>
     );
 };
 
